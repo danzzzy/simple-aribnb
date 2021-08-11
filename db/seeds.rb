fb21ec250ada4b9493bec353f52556ff0c85e8c7
Daniel Rabottini <da_nkr@live.com>
2021-08-11 16:59:52 +0900

    flats exercise logic created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "deleting current db"
+
+Flat.delete_all
+
+puts "creating db"
+
+15.times do
+  Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_name,
+    description: ["beautiful place", "cozy place", "friendly neighborhood"].sample,
+    price_per_night: rand(100..599),
+    number_of_guests: rand(1..12)
+
+  )
+end
+
+puts "db created #{Flat.count}"
